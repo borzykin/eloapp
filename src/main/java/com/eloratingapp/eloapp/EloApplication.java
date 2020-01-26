adb55df034fedc56664af0b8953d0dc7f5b58b09
Oleksii Borzykin <borzykin3455@gmail.com>
2020-01-26 23:12:28 +0200

    Initial commit Project is created with spring initializr

---
+++ src/main/java/com/eloratingapp/eloapp/EloApplication.java
@@ -0,0 +1,13 @@
+package com.eloratingapp.eloapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EloApplication.class, args);
+	}
+
+}
